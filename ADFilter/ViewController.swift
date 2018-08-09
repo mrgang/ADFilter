e0a333e50c7df7c463b9e8ca01c222b8e99a8c9d
ligang <li93gang@163.com>
2018-08-09 09:41:50 +0800

    Initial Commit

---
+++ ADFilter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ADFilter
+//
+//  Created by 李刚 on 2018/8/9.
+//  Copyright © 2018年 sunsheen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
